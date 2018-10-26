0d3f68b4b6b0ab51f754cf67aa4e9af20c8bffb5
Kristen Fairman <fairman.kristen@gmail.com>
2018-10-26 09:30:43 -0500

    Port Changed

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 var path = require("path");
 // Set the port of our application
 // process.env.PORT lets the port be set by Heroku
-const port = process.env.PORT || 4000;
+// const port = process.env.PORT || 4000;
 var PORT = process.env.PORT || 8080;
 
 // Create express app instance.
@@ -22,7 +22,7 @@ require(path.join(__dirname, '/app/routing/apiRoutes'))(app);
 require(path.join(__dirname, '/app/routing/htmlRoutes'))(app);
 // require(path.join(__dirname, './app/data/friends'))(app);
 
-app.listen(port);
+// app.listen(port);
 app.listen(PORT, function() {
     // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT);
